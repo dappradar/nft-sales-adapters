194eea189a28c6a4ac48cb625ec88c0b8def5fe2
Liutauras Januškas <82936197+liutaurasjanuskas@users.noreply.github.com>
2022-03-10 16:41:00 +0200

    Divide native price by decimals for `bazaar` provider

--- adapters/bazaar/index.js
+++ adapters/bazaar/index.js
@@ -6,13 +6,14 @@ const axios = require("axios");
 const URL = "http://nft-sales-service.dappradar.com/open-source";
 const KEY = process.env.DAPPRADAR_API_KEY;
 const path = require("path");
+const BigNumber = require("bignumber.js");
 
 class Bazaar {
     constructor() {
         this.name = "bazaar";
         this.symbol = "SKILL";
         this.token = "0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab";
-        this.protocol = "bsc";
+        this.protocol = "binance-smart-chain";
         this.block = 8114624;
         this.contract = "0x90099da42806b21128a094c713347c7885af79e2";
         this.events = ["PurchasedListing"];
@@ -79,7 +80,7 @@ class Bazaar {
         const block = await this.sdk.getBlock(event.blockNumber);
         const timestamp = moment.unix(block.timestamp).utc();
         const po = await this.getPrice(block.timestamp);
-        const nativePrice = +event.returnValues.price;
+        const nativePrice = new BigNumber(event.returnValues.price).dividedBy(10 ** this.symbol.decimals);
         const buyer = await this.getBuyer(event);
         if (!buyer) {
             return;
@@ -89,14 +90,14 @@ class Bazaar {
         const entity = {
             provider_name: this.name,
             provider_contract: this.contract,
-            protocol: "binance-smart-chain",
+            protocol: this.protocol,
             nft_contract: this.contract,
             nft_id: tokenId,
             token: this.token,
             token_symbol: this.symbol.symbol,
             amount: 1,
-            price: +nativePrice,
-            price_usd: +nativePrice * +po.price,
+            price: nativePrice.toNumber(),
+            price_usd: nativePrice.multipliedBy(po.price).toNumber(),
             seller: this.contract,
             buyer: buyer.toLowerCase(),
             sold_at: timestamp.format("YYYY-MM-DD HH:mm:ss"),
@@ -107,7 +108,6 @@ class Bazaar {
     };
 
     addToDatabase = async entity => {
-        // console.log(`creating sale for ${entity.nft_contract} with id ${entity.nft_id}`);
         return entity;
     };
 }
