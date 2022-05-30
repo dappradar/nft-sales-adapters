f27e778bbcddd86ca614105296b86703535b8597
weberkla <klaus.weber@informatik.uni-augsburg.de>
2022-05-31 00:44:09 +0200

    reverted to standard index file.

--- adapters/labrats/index.js
+++ adapters/labrats/index.js
@@ -35,7 +35,6 @@ class LABRATS {
     loadSdk = () => {
         return new BSC(this);
     };
-
     getSymbol = async () => {
         const resp = await axios.get(
             `${URL}/token-metadata?key=${KEY}&token_address=${this.token}&protocol=${this.protocol}`,
@@ -46,9 +45,9 @@ class LABRATS {
                 },
             },
         );
-        return resp.data;
+        const symbol = resp.data;
+        return symbol;
     };
-
     getPrice = async timestamp => {
         const resp = await axios.get(
             `${URL}/token-price?key=${KEY}&token_address=${this.token}&protocol=${this.protocol}&timestamp=${timestamp}`,
@@ -61,14 +60,13 @@ class LABRATS {
         );
         return resp.data;
     };
-
     stop = async () => {
         this.sdk.stop();
     };
 
     getBuyer = async event => {
         const buyer = event.returnValues.owner;
-        if (event.event === "PurchasedListing") {
+        if (event.event === "NameRenewed") {
             const txReceipt = await this.sdk.getTransactionReceipt(event.transactionHash);
             if (txReceipt === null) {
                 return null;
@@ -78,44 +76,31 @@ class LABRATS {
         return buyer;
     };
 
-    _getPrice = async (event, block) => {
-        if (!this.symbol.decimals) {
-            return { price: null, priceUsd: null };
-        }
-
-        const po = await this.getPrice(block.timestamp);
-        const nativePrice = new BigNumber(event.returnValues.price).dividedBy(10 ** this.symbol.decimals);
-
-        return {
-            price: nativePrice.toNumber(),
-            priceUsd: nativePrice.multipliedBy(po.price).toNumber(),
-        };
-    };
-
     process = async event => {
         const block = await this.sdk.getBlock(event.blockNumber);
         const timestamp = moment.unix(block.timestamp).utc();
+        const po = await this.getPrice(block.timestamp);
+        const nativePrice = new BigNumber(event.returnValues.cost).dividedBy(10 ** this.symbol.decimals);
         const buyer = await this.getBuyer(event);
         if (!buyer) {
             return;
         }
 
-        const { price, priceUsd } = await this._getPrice(event, block);
-
-        const tokenId = event.returnValues.nftID;
+        const labelHash = event.returnValues.label;
+        const tokenId = new BigNumber(labelHash, 16).toFixed();
         const entity = {
-            provider_name: this.name,
-            provider_contract: this.contract,
+            provider_name: this.name, // the name of the folder
+            provider_contract: this.contract, // the providers contract from which you get data
             protocol: this.protocol,
             nft_contract: this.contract,
             nft_id: tokenId,
             token: this.token,
             token_symbol: this.symbol.symbol,
             amount: 1,
-            price,
-            price_usd: priceUsd,
-            seller: this.contract,
-            buyer: buyer.toLowerCase(),
+            price: nativePrice.toNumber(),
+            price_usd: nativePrice.multipliedBy(po.price).toNumber(),
+            seller: this.contract, // its bought from ens and transfered to the owner
+            buyer,
             sold_at: timestamp.format("YYYY-MM-DD HH:mm:ss"),
             block_number: event.blockNumber,
             transaction_hash: event.transactionHash,
@@ -124,6 +109,7 @@ class LABRATS {
     };
 
     addToDatabase = async entity => {
+        console.log(`creating sale for ${entity.nft_contract} with id ${entity.nft_id}`);
         return entity;
     };
 }
