77484c74ac7a6fa8a7c11d67234d896b7ae260b9
namchheavcoin <56229126+Namchheavcoin@users.noreply.github.com>
2022-07-01 14:30:41 +0700

    Update avalanche.js

--- sdk/avalanche.js
+++ sdk/avalanche.js
@@ -3,11 +3,11 @@ const { AVAILABLE_PROTOCOLS } = require("./constants");
 
 const EVMC = require("./EVMC");
 
-module.exports = class kalao-avalanche extends EVMC {
+module.exports = class kalao_avalanche extends EVMC {
     constructor(props) {
         super(props);
         this.range = 500;
         this.chunkSize = 2;
-        this.protocol = AVAILABLE_PROTOCOLS.kalao-avalanche;
+        this.protocol = AVAILABLE_PROTOCOLS.kalao_avalanche;
     }
 };
