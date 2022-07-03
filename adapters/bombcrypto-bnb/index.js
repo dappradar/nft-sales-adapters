155f3bfd523f0729c9e6b5463de865d36e35e19e
Ankit Singh <ankit.singh.zm@gmail.com>
2022-07-03 13:07:36 +0530

    fixed Contract Address

--- adapters/bombcrypto-bnb/index.js
+++ adapters/bombcrypto-bnb/index.js
@@ -15,7 +15,7 @@ class BombCrypto {
         this.token = "bnb";
         this.protocol = "binance";
         this.block = 8867059;
-        this.contract = "0x5713Ae21F4Bb696A877c90CCcAE310eFF4c4652A"; // Original Contract - 0xe29f0b490f0d89ca7acac1c7bed2e07ecad65201, here it's reading contract as proxy
+        this.contract = "0xe29f0b490f0d89ca7acac1c7bed2e07ecad65201"; // abi used for 0x5713Ae21F4Bb696A877c90CCcAE310eFF4c4652A(Proxy Contract abi) - https://bscscan.com/address/0xe29F0B490F0d89CA7ACAc1C7BeD2E07eCAD65201#readProxyContract
         this.events = ["ItemListed", "ItemPriceUpdated", "OwnershipTransferred"];
         this.pathToAbi = path.join(__dirname, "./abi.json");
         this.range = 500;
