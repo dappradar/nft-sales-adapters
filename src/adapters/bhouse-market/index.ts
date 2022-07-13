5b305d6550f942a2c111b50a813482963ae406af
Rodrigo Kreutz <8869678+rkreutz@users.noreply.github.com>
2022-07-13 11:19:22 +0100

    Update src/adapters/bhouse-market/index.ts

--- src/adapters/bhouse-market/index.ts
+++ src/adapters/bhouse-market/index.ts
@@ -66,7 +66,7 @@ class BHouseMarket {
         return event.returnValues.buyer;
     };
 
-    getSeller = async (event: EventData): Promise<string | null> => {
+    getSeller = (event: EventData): string | null => {
         return event.returnValues.seller;
     };
 
