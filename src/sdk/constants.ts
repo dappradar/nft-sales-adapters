be6490e046fb69451abf03921aa57b90c8d203a1
chris guo <chrisguox@gmail.com>
2022-08-30 13:42:36 +0800

    refactor: adjust the judgement order for const

--- src/sdk/constants.ts
+++ src/sdk/constants.ts
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 enum AVAILABLE_PROTOCOLS {
     ETH = "ethereum",
     BSC = "bsc",
@@ -7,13 +9,15 @@ enum AVAILABLE_PROTOCOLS {
 }
 
 const API_KEY = process.env.DAPPRADAR_API_KEY || "";
-const API_URL = process.env.DAPPRADAR_API_URL || "";
-const WS_PROXY_URL = process.env.WS_PROXY_URL || "";
-const HTTP_PROXY_URL = process.env.HTTP_PROXY_URL || "";
-
 if (!API_KEY.length) throw new Error(`Missing "API_KEY" environment variable`);
+
+const API_URL = process.env.DAPPRADAR_API_URL || "";
 if (!API_URL.length) throw new Error(`Missing "API_URL" environment variable`);
+
+const WS_PROXY_URL = process.env.WS_PROXY_URL || "";
 if (!WS_PROXY_URL.length) throw new Error(`Missing "WS_PROXY_URL" environment variable`);
+
+const HTTP_PROXY_URL = process.env.HTTP_PROXY_URL || "";
 if (!HTTP_PROXY_URL.length) throw new Error(`Missing "HTTP_PROXY_URL" environment variable`);
 
 const DEFAULT_MAX_RETRIES = 10;
