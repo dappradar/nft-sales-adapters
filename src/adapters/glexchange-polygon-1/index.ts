cd939eb356e65569fe2d2a407ea59a3416adb94b
Selvam Ramsamy <selvam@blockchainappfactory.com>
2022-11-01 22:06:11 +0530

    initial commit

---
+++ src/adapters/glexchange-polygon-1/index.ts
@@ -0,0 +1,92 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import BigNumber from "bignumber.js";
+import moment from "moment";
+import { EventData } from "web3-eth-contract";
+import path from "path";
+import priceSdk from "../../sdk/price";
+import Matic from "../../sdk/matic";
+import symbolSdk from "../../sdk/symbol";
+import { ISaleEntity, ISymbolAPIResponse } from "../../sdk/Interfaces";
+
+class GlExchange {
+    name: string;
+    token: string;
+    protocol: string;
+    block: number;
+    contract: string;
+    events: string[];
+    pathToAbi: string;
+    range: number;
+    chunkSize: number;
+    sdk: any;
+
+    constructor() {
+        this.name = "glexchange-polygon-1";
+        this.protocol = "polygon";
+	this.token = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
+        this.block =  34825626;
+        this.contract = "0x8013bf7e0278891FB82b26FbD56628e12FEd112A";
+        this.events = ["OrderExecuted"];
+        this.pathToAbi = path.join(__dirname, "./abi.json");
+        this.range = 500;
+        this.chunkSize = 6;
+    }
+
+    run = async (): Promise<void> => {
+        this.sdk = await this.loadSdk();
+
+        await this.sdk.run();
+    };
+
+    loadSdk = (): any => {
+        return new Matic(this);
+    };
+
+    stop = async (): Promise<void> => {
+        this.sdk.stop();
+    };
+
+    process = async (event: EventData): Promise<void> => {
+        const block = await this.sdk.getBlock(event.blockNumber);
+        const timestamp = moment.unix(block.timestamp).utc();
+        const token = this.token;
+        const symbol: ISymbolAPIResponse = await symbolSdk.get(token, this.protocol);
+        const po = await priceSdk.get(token, this.protocol, block.timestamp);
+        const amount = event.returnValues["supply"];
+        const price = event.returnValues["price"];
+        const nativePrice = new BigNumber(price).dividedBy(10 ** (symbol?.decimals || 0));
+        const seller = event.returnValues["seller"];
+        const buyer = event.returnValues["buyer"];
+        const nftContract = event.returnValues["nftAddress"];
+        const tokenId = event.returnValues["tokenId"];
+        const entity = {
+            providerName: this.name,
+            providerContract: this.contract,
+            protocol: this.protocol,
+            nftContract: nftContract.toLowerCase(),
+            nftId: tokenId,
+            token: token.toLowerCase(),
+            tokenSymbol: symbol?.symbol || "",
+            amount,
+            price: nativePrice.toNumber(),
+            priceUsd: null === symbol.decimals ? 0 : nativePrice.multipliedBy(po.price).toNumber(),
+            seller: seller.toLowerCase(),
+            buyer: buyer.toLowerCase(),
+            soldAt: timestamp.format("YYYY-MM-DD HH:mm:ss"),
+            blockNumber: event.blockNumber,
+            transactionHash: event.transactionHash,
+        };
+
+        await this.addToDatabase(entity);
+    };
+
+    addToDatabase = async (entity: ISaleEntity): Promise<ISaleEntity> => {
+        console.log(`creating sale for ${entity.nftContract} with id ${entity.nftId}`);
+        return entity;
+    };
+}
+
+export default GlExchange;
