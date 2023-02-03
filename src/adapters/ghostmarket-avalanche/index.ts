b7fd8887d372a9fb2b05059a6af513782c1cbf84
NeoDashboard <neo.blockchain.dashboard@gmail.com>
2023-02-03 14:51:32 +0100

    Add GhostMarket adapter

---
+++ src/adapters/ghostmarket-avalanche/index.ts
@@ -0,0 +1,101 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import BigNumber from "bignumber.js";
+import moment from "moment";
+import { EventData } from "web3-eth-contract";
+import path from "path";
+import priceSdk from "../../sdk/price";
+import Avalanche from "../../sdk/avalanche";
+import symbolSdk from "../../sdk/symbol";
+import { ISaleEntity, ISymbolAPIResponse } from "../../sdk/Interfaces";
+
+class Element {
+    name: string;
+    token: string;
+    protocol: string;
+    block: number;
+    deprecatedAtBlock: number;
+    contract: string;
+    events: string[];
+    pathToAbi: string;
+    range: number;
+    chunkSize: number;
+    sdk: any;
+
+    constructor() {
+        this.name = "ghostmarket-avalanche";
+        this.protocol = "avalanche";
+        this.block = 15385077;
+        this.contract = "0xeb4aba7aeba732fc2fc92a673585d950ccfc1de0";
+        this.events = ["OrderFilled"];
+        this.pathToAbi = path.join(__dirname, "./abi.json");
+        this.range = 500;
+        this.chunkSize = 6;
+    }
+
+    run = async (): Promise<void> => {
+        this.sdk = await this.loadSdk();
+
+        await this.sdk.run();
+    };
+
+    loadSdk = (): any => {
+        return new Avalanche(this);
+    };
+
+    stop = async (): Promise<void> => {
+        this.sdk.stop();
+    };
+
+    _getToken = (event: EventData): string => {
+        let token = "avax";
+        if (event.returnValues["rightAsset"]["data"] != "0x") {
+            token = event.returnValues["rightAsset"]["data"][0].toLowerCase();
+        }
+
+        return token;
+    };
+
+    process = async (event: EventData): Promise<void> => {
+        const block = await this.sdk.getBlock(event.blockNumber);
+        const timestamp = moment.unix(block.timestamp).utc();
+        const token = this._getToken(event);
+        const symbol: ISymbolAPIResponse = await symbolSdk.get(token, this.protocol);
+        const po = await priceSdk.get(token, this.protocol, block.timestamp);
+        const price = event.returnValues["newLeftFill"];
+        const nativePrice = new BigNumber(price).dividedBy(10 ** (symbol?.decimals || 0));
+        const seller = event.returnValues["leftMaker"];
+        const buyer = event.returnValues["rightMaker"];
+        const nftContract = event.returnValues["leftAsset"]["data"][0];
+        const tokenId = event.returnValues["leftAsset"]["data"][1];
+
+        const entity = {
+            providerName: this.name,
+            providerContract: this.contract,
+            protocol: this.protocol,
+            nftContract: nftContract.toLowerCase(),
+            nftId: tokenId,
+            token: token.toLowerCase(),
+            tokenSymbol: symbol?.symbol || "",
+            amount: 1,
+            price: nativePrice.toNumber(),
+            priceUsd: !symbol?.decimals ? null : nativePrice.multipliedBy(po.price).toNumber(),
+            seller: seller.toLowerCase(),
+            buyer: buyer.toLowerCase(),
+            soldAt: timestamp.format("YYYY-MM-DD HH:mm:ss"),
+            blockNumber: event.blockNumber,
+            transactionHash: event.transactionHash,
+        };
+
+        await this.addToDatabase(entity);
+    };
+
+    addToDatabase = async (entity: ISaleEntity): Promise<ISaleEntity> => {
+        console.log(`creating sale for ${entity.nftContract} with id ${entity.nftId}`);
+        return entity;
+    };
+}
+
+export default Element;
---
+++ src/adapters/ghostmarket-bsc/index.ts
@@ -0,0 +1,102 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import BigNumber from "bignumber.js";
+import moment from "moment";
+import { EventData } from "web3-eth-contract";
+import path from "path";
+import priceSdk from "../../sdk/price";
+import Binance from "../../sdk/binance";
+import symbolSdk from "../../sdk/symbol";
+import { ISaleEntity, ISymbolAPIResponse } from "../../sdk/Interfaces";
+
+class Element {
+    name: string;
+    token: string;
+    protocol: string;
+    block: number;
+    deprecatedAtBlock: number;
+    contract: string;
+    events: string[];
+    pathToAbi: string;
+    range: number;
+    chunkSize: number;
+    sdk: any;
+
+    constructor() {
+        this.name = "ghostmarket-bsc";
+        this.protocol = "binance-smart-chain";
+        this.block = 18259152;
+        this.contract = "0x388171f81fc91efc7338e07e52555a90c7d87972";
+        this.events = ["OrderFilled"];
+        this.pathToAbi = path.join(__dirname, "./abi.json");
+        this.range = 500;
+        this.chunkSize = 6;
+    }
+
+    run = async (): Promise<void> => {
+        this.sdk = await this.loadSdk();
+
+        await this.sdk.run();
+    };
+
+    loadSdk = (): any => {
+        return new Binance(this);
+    };
+
+    stop = async (): Promise<void> => {
+        this.sdk.stop();
+    };
+
+    _getToken = (event: EventData): string => {
+        let token = "bnb";
+        if (event.returnValues["rightAsset"]["data"] != "0x") {
+            token = this.sdk.web3.eth.abi.decodeParameter('address', event.returnValues["rightAsset"]["data"]).toLowerCase();
+        }
+
+        return token;
+    };
+
+    process = async (event: EventData): Promise<void> => {
+        const block = await this.sdk.getBlock(event.blockNumber);
+        const timestamp = moment.unix(block.timestamp).utc();
+        const token = this._getToken(event);
+        const symbol: ISymbolAPIResponse = await symbolSdk.get(token, this.protocol);
+        const po = await priceSdk.get(token, this.protocol, block.timestamp);
+        const price = event.returnValues["newLeftFill"];
+        const nativePrice = new BigNumber(price).dividedBy(10 ** (symbol?.decimals || 0));
+        const seller = event.returnValues["leftMaker"];
+        const buyer = event.returnValues["rightMaker"];
+        let params = this.sdk.web3.eth.abi.decodeParameters(['address', 'uint256'], event.returnValues["leftAsset"]["data"]);
+        let nftContract = params[0];
+        let tokenId = params[1];
+
+        const entity = {
+            providerName: this.name,
+            providerContract: this.contract,
+            protocol: this.protocol,
+            nftContract: nftContract.toLowerCase(),
+            nftId: tokenId,
+            token: token.toLowerCase(),
+            tokenSymbol: symbol?.symbol || "",
+            amount: 1,
+            price: nativePrice.toNumber(),
+            priceUsd: !symbol?.decimals ? null : nativePrice.multipliedBy(po.price).toNumber(),
+            seller: seller.toLowerCase(),
+            buyer: buyer.toLowerCase(),
+            soldAt: timestamp.format("YYYY-MM-DD HH:mm:ss"),
+            blockNumber: event.blockNumber,
+            transactionHash: event.transactionHash,
+        };
+
+        await this.addToDatabase(entity);
+    };
+
+    addToDatabase = async (entity: ISaleEntity): Promise<ISaleEntity> => {
+        console.log(`creating sale for ${entity.nftContract} with id ${entity.nftId}`);
+        return entity;
+    };
+}
+
+export default Element;
