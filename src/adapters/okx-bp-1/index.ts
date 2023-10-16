6a51d88cbcbc0fc6d959e10d31b70f38d83c143b
Liutauras Januškas <82936197+liutaurasjanuskas@users.noreply.github.com>
2023-10-16 11:51:25 +0300

    Add OKX reusable provider

---
+++ src/adapters/okx-bp-1/index.ts
@@ -0,0 +1,78 @@
+import BigNumber from "bignumber.js";
+import moment from "moment";
+import {EventData} from "web3-eth-contract";
+import {ISaleEntity} from "../../sdk/Interfaces";
+import BasicProvider, {IBasicProviderOptions} from "../../sdk/basic-provider";
+
+class OKX extends BasicProvider {
+    defaultPaymentToken: string;
+
+    constructor(options: IBasicProviderOptions) {
+        super(options);
+
+        if (!this.defaultPaymentToken) {
+            throw new Error(`Missing default payment token for provider "${this.name}"`);
+        }
+
+        this.name = "okx-avalanche-1";
+        this.block = 26572369;
+        this.contract = "0xa88ba7ea2912cde5d632c8de048a17f2ea3e75a0";
+        this.events = ["MatchOrderResultsV3"];
+    }
+
+    private getToken = (item: any): string => {
+        if (item[2] === "0x0000000000000000000000000000000000000000") {
+            return this.defaultPaymentToken;
+        }
+
+        return item[2].toLowerCase();
+    };
+
+    private processItem = async (event: EventData, item: any): Promise<void> => {
+        const [actionType, price, payToken, nftContract, tokenId, amount, tradeType, extraData] = item;
+        const token = this.getToken(item);
+        const maker = extraData.substring(0, 42);
+        const taker = `0x${extraData.substring(42, 82)}`;
+        const isAcceptOffer = Number(actionType) === 3;
+        const block = await this.sdk.getBlock(event.blockNumber);
+        const timestamp = moment.unix(block.timestamp).utc();
+        const buyer = isAcceptOffer ? maker : taker;
+        const seller = isAcceptOffer ? taker : maker;
+
+        const entity = {
+            providerName: this.name,
+            providerContract: this.contract,
+            nfts: [
+                {
+                    id: tokenId,
+                    amount: Number(amount),
+                    contract: nftContract.toLowerCase(),
+                },
+            ],
+            token,
+            price: new BigNumber(price),
+            seller: seller.toLowerCase(),
+            buyer: buyer.toLowerCase(),
+            soldAt: timestamp,
+            blockNumber: event.blockNumber,
+            transactionHash: event.transactionHash,
+            chainId: this.sdk.chainId,
+        };
+
+        await this.addToDatabase(entity);
+    };
+
+    process = async (event: EventData): Promise<void> => {
+        const params = event.returnValues.params;
+
+        for (let i = 0; i < params.length; i++) {
+            await this.processItem(event, params[i]);
+        }
+    };
+
+    addToDatabase = async (entity: ISaleEntity): Promise<ISaleEntity> => {
+        return entity;
+    };
+}
+
+export default OKX;
