58939be8d8e2983ae134a8d1e1b8635ab4d1438d
Luke Choi <lukechoi@softnyx.com>
2024-02-19 07:52:14 +0900

    add kingdomstory-market-bsc-1 adapter

---
+++ src/adapters/kingdomstory-market-bp-1/index.ts
@@ -0,0 +1,66 @@
+import moment from "moment";
+import BigNumber from "bignumber.js";
+import {ISaleEntity} from "../../sdk/Interfaces";
+import {EventData} from "web3-eth-contract";
+import BasicProvider, {IBasicProviderOptions} from "../../sdk/basic-provider";
+
+// event NewSale(
+//     address indexed seller,
+//     address indexed buyer,
+//     uint256 indexed tokenId,
+//     uint256 totalPricePaid,
+//     address currencyContract,
+//     address nftContract,
+//     uint256 tradeId
+// );
+
+class KingdomStoryMarket extends BasicProvider {
+    constructor(options: IBasicProviderOptions) {
+        super(options);
+        this.events = ["NewSale"];
+    }
+
+    process = async (event: EventData): Promise<ISaleEntity> => {
+        const block = await this.sdk.getBlock(event.blockNumber);
+        const timestamp = moment.unix(block.timestamp).utc();
+        // const token = await this.sdk.callContractMethod("bcoinContract", [], undefined, event.blockNumber);
+        const token = event.returnValues.currencyContract;
+
+        if (!token) {
+            throw new Error(`Failed to fetch token address for transaction "${event.transactionHash}". Provider - "${this.name}"`);
+        }
+
+        // const nftContract = await this.sdk.callContractMethod("nftContract", [], undefined, event.blockNumber);
+        const nftContract = event.returnValues.nftContract;
+
+        if (!nftContract) {
+            throw new Error(`Failed to fetch NFT contract address for transaction "${event.transactionHash}". Provider - "${this.name}"`);
+        }
+
+        const entity: ISaleEntity = {
+            providerName: this.name,
+            providerContract: this.contract,
+            nfts: [{
+                contract: nftContract.toLowerCase(),
+                id: event.returnValues.tokenId,
+                amount: 1
+            }],
+            token: token.toLowerCase(),
+            price: new BigNumber(event.returnValues.totalPricePaid),
+            seller: event.returnValues.seller.toLowerCase(),
+            buyer: event.returnValues.buyer.toLowerCase(),
+            soldAt: timestamp,
+            blockNumber: event.blockNumber,
+            transactionHash: event.transactionHash,
+            chainId: this.sdk.chainId,
+        };
+
+        return this.addToDatabase(entity);
+    };
+
+    addToDatabase = async (entity: ISaleEntity): Promise<ISaleEntity> => {
+        return entity;
+    };
+}
+
+export default KingdomStoryMarket;
--- src/config/adapters.ts
+++ src/config/adapters.ts
@@ -194,6 +194,13 @@ const BSC: IBasicProviderOptions[] = [
         chainId: 15,
         defaultPaymentToken: "0xd069599e718f963bd84502b49ba8f8657faf5b3a"
     },
+    {
+        name: "kingdomstory-market-bsc-1",
+        basicProvider: "kingdomstory-market-bp-1",
+        block: 31877965,
+        contract: "0x0e37943581489f7771981bb5ca882b474d0d5ad2",
+        chainId: 15,
+    },
 ];
 
 const POLYGON: IBasicProviderOptions[] = [
