f6e751ee4448b4083f7dce158412168c867c1498
Liutauras Januškas <82936197+liutaurasjanuskas@users.noreply.github.com>
2025-09-09 15:52:25 +0300

    Add missing SDK

---
+++ src/sdk/skale-nebula.ts
@@ -0,0 +1,19 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import { AVAILABLE_PROTOCOLS } from "./constants";
+import EVMC_HTTP from "./EVMC-HTTP";
+
+class SkaleNebula extends EVMC_HTTP {
+    constructor(props: any) {
+        super(props);
+
+        this.protocol = AVAILABLE_PROTOCOLS.SKALE_NEBULA;
+        this.chainId = 87;
+        this.node = process.env.SKALE_NEBULA_NODE_HTTP;
+        this.range = 500;
+    }
+}
+
+export default SkaleNebula;
